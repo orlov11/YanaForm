2fcb87bb6e12290e4fa7147191a0017e25169794
Svyatoslav Orlov <svyatoslav.orlov11@gmail.com>
2020-09-08 21:49:22 +0300

    commit 3.2 add JS

---
+++ script/script.js
@@ -0,0 +1,40 @@
+const modal = document.querySelector('.modal-pop'),
+      btn = document.querySelector('form'),
+      btnClosed = document.querySelector('#btn1');
+
+function showModal(){
+    modal.classList.add('show');
+    modal.classList.remove('hide');
+    document.body.style.overflow = 'hidden';
+
+}
+
+function hideModal() {
+    modal.classList.add('hide');
+    modal.classList.remove('show');
+
+}
+
+modal.addEventListener('click', (e)=>{
+    if(e.target == modal){
+        hideModal();
+    }
+
+});
+
+btn.addEventListener('submit', (e) =>{
+    e.preventDefault();
+    showModal();
+});
+
+btnClosed.addEventListener('click', (e) =>{
+    btnClosed.classList.add('blue');
+    hideModal();
+});
+
+document.addEventListener('keydown',  (e)=>{
+    if(e.code === 'Escape' && modal.classList.contains('show')){
+        hideModal();
+    }
+
+});
