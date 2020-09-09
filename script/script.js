c0f1522eebc96d95d03555205f407cc9fa98ba8d
Svyatoslav Orlov <svyatoslav.orlov11@gmail.com>
2020-09-09 14:38:30 +0300

    commit 3.2.6

--- script/script.js
+++ script/script.js
@@ -1,42 +1,47 @@
-const modal = document.querySelector('.modal-pop'),
-      btn = document.querySelector('form'),
-      btnClosed = document.querySelector('#btn1');
+window.addEventListener('DOMContentLoaded', () =>{
+    
+    const modal = document.querySelector('.modal-pop'),
+        btn = document.querySelector('form'),
+        btnClosed = document.querySelector('#btn1'),
+        check =  document.querySelector('input[type=checkbox]');
 
-function showModal(){
-    modal.classList.add('show');
-    modal.classList.remove('hide');
-    document.body.style.overflow = 'hidden';
-
-}
-
-function hideModal() {
-    modal.classList.add('hide');
-    modal.classList.remove('show');
-    document.body.style.overflow = '';
-
-}
-
-
-modal.addEventListener('click', (e)=>{
-    if(e.target == modal){
-        hideModal();
+    function showModal(){
+        modal.classList.add('show');
+        modal.classList.remove('hide');
     }
 
-});
-
-btn.addEventListener('submit', (e) =>{
-    e.preventDefault();
-    showModal();
-    // setTimeout(hideModal, 3000)
-});
-
-btnClosed.addEventListener('click', (e) =>{
-    hideModal();
-});
-
-document.addEventListener('keydown',  (e)=>{
-    if(e.code === 'Escape' && modal.classList.contains('show')){
-        hideModal();
+    function hideModal() {
+        modal.classList.add('hide');
+        modal.classList.remove('show');
+        btn.reset();
     }
+
+
+    modal.addEventListener('click', (e)=>{
+        if(e.target == modal){
+            hideModal();
+        }
+    });
+
+    btn.addEventListener('submit', (e) =>{
+        e.preventDefault();
+        if(check.checked){
+            showModal();
+        } else{
+            alert('Сначала нужно согласится с условиями');
+        }
+    });
+
+    btnClosed.addEventListener('click', (e) =>{
+        hideModal();
+    });
+
+    document.addEventListener('keydown',  (e)=>{
+        if(e.code === 'Escape' && modal.classList.contains('show')){
+            hideModal();
+        }
+    });
+
+
 
 });
